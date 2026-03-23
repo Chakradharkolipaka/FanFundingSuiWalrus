9387ac5d82a97efc287abfa36b5770d715f0f4a7
Chakradhar Kolipaka <chakradharkolipaka@gmail.com>
2026-03-23 09:58:09 +0000

    chore: remove test file

--- test-key.ts
+++
@@ -1,4 +0,0 @@
-import { Ed25519Keypair } from "@mysten/sui/keypairs/ed25519";
-const kp = new Ed25519Keypair();
-console.log("Secret key format:", kp.getSecretKey());
-console.log("Length:", kp.getSecretKey().length);
